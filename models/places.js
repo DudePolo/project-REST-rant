c0288a582a87cc8bd3e01dfff58c88fe60971fc3
DudePolo <andres.polo.wood@gmail.com>
2024-02-04 13:58:26 -0500

    added place schema

--- models/places.js
+++ models/places.js
@@ -1,20 +1,32 @@
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  pic: String,
+  cuisines: { type: String, require: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
 //Get Places
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/mcds.jpg',
-    cred: "https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-    credName: 'Andres',
-    photoLink: ""
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/bk.jpg',
-    cred: "https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-    credName: 'Andress',
-    photoLink: ""
-  }]
+// [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/mcds.jpg',
+//     cred: "https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+//     credName: 'Andres',
+//     photoLink: ""
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/bk.jpg',
+//     cred: "https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+//     credName: 'Andress',
+//     photoLink: ""
+//   }]
