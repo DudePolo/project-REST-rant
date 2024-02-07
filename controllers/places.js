a79148c1d32149bd008fc9ff941fc5935d2b2bb2
Andres Polo-Wood <andres.polo.wood@gmail.com>
2024-02-06 21:06:29 -0500

    edited show page

--- controllers/places.js
+++ controllers/places.js
@@ -32,7 +32,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
     })
     .catch(err => {
@@ -60,6 +62,28 @@ router.post('/:id/rant', (req, res) => {
 
 router.delete('/:id/rant/:rantID', (req, res) => {
   res.send('GET /places/:id/rant/:rantID stub')
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+    .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+        place.comments.push(comment.id)
+        place.save()
+        .then(() => {
+          res.redirect(`/places/${req.params.id}`)
+        })
+      })
+      .catch(err => {
+        res.render('error404')
+      })
+    })
+    .catch(err => {
+      res.render('error404')
+    })
 })
 
 module.exports = router
